4e9b26c5eb599c3c78d046d1b5062ba966025a0b
Nick de Kruijk <git@nickdekruijk.nl>
2018-01-26 13:26:05 +0100

    Simple frontend view

--- src/views/main.blade.php
+++ src/views/main.blade.php
@@ -2,19 +2,74 @@
 <html>
     <head>
         <meta charset="utf-8">
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Hello Bulma!</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.min.css">
-        <script defer src="https://use.fontawesome.com/releases/v5.0.0/js/all.js"></script>
+        <meta name="description" content="{{ @$page->description }}">
+    	<title>{{ @$page->html_title }}</title>
+    	<style>
+        	body {margin:0;font-family:sans-serif;color:#333;line-height:1.5;-webkit-font-smoothing:antialiased;-moz-font-smoothing:grayscale;-moz-osx-font-smoothing:grayscale}
+        	.nav {position:fixed;left:0;right:0;top:0;background-color:rgba(127,127,127,0.2);line-height:50px;white-space:nowrap;z-index:1000}
+        	* {box-sizing:border-box}
+        	.content {min-height:calc(100vh - 100px)}
+        	.container {position:relative}
+        	.footer .container,
+        	.article {padding:1px 2%}
+        	.footer {line-height:100px;background-color:#334;color:rgba(255,255,255,0.8)}
+        	A {text-decoration:none;color:inherit}
+        	.nav-logo {font-weight:100;font-size:1.5em}
+        	.mw-1140 {max-width:1140px;margin-left:auto;margin-right:auto}
+        	.nav-burger {display:none;position:absolute;right:0;padding:11px 2%;z-index:1}
+        	.nav-burger > span {display:block;background-color:#0cb;height:4px;width:22px;margin:4px;border-radius:2px}
+        	#nav-toggle {display:none}
+        	.nav UL {display:block;margin:0;padding:0}
+        	.nav UL > LI {display:inline-block;margin:0;padding:0px 10px}
+        	.nav UL UL {display:none;position:absolute;background-color:rgba(127,127,127,0.2);margin:0 -10px;line-height:2.5em}
+        	.nav LI:hover > UL {display:block}
+        	.nav LI LI {display:block}
+        	.nav LI.active > A {color:#178}
+        	.nav A {display:block}
+        	.nav LI:hover, .nav-logo:hover {background-color:rgba(0,0,0,0.1)}
+        	.nav-logo {float:left;display:block;padding:0px 2%;color:#178}
+        	.right {float:right}
+        	.bigimg {height:33.33333vw;background-position:center center;background-size:cover}
+        	.footer a {color:#fff}
+        	.footer a:hover {text-decoration:underline}
+            @media (max-width: 750px) {
+            	.nav .nav0 {position:fixed;top:0px;bottom:0;right:0;width:200px;transform:translateX(200px);transition:transform .4s;background-color:#eee}
+            	.nav .nav0 > LI {display:block}
+            	.nav UL UL {position:relative;display:block;background-color:inherit}
+            	.nav LI LI {padding-left:20px;font-size:0.95em}
+            	.nav-burger {display:block}
+                .nav LI:hover, .nav-logo:hover {background-color:inherit}
+                #nav-toggle:checked ~ .nav .nav0 {display:block;transform:translateX(0)}
+        	}
+        </style>
     </head>
     <body>
-        <section class="section">
-            <div class="container">
-                <h1 class="title">Hello World</h1>
-                <p class="subtitle">
-                    My first website with <strong>Bulma</strong>!
-                </p>
+        <input type="checkbox" id="nav-toggle">
+        <nav class="nav" role="navigation" aria-label="main navigation" class="nav" id="nav">
+            <a class="nav-logo" href="/">LaraPages</a>
+            <label class="nav-burger" for="nav-toggle">
+                <span></span>
+                <span></span>
+                <span></span>
+            </label>
+            <div class="mw-1140">
+            {!! $navigationHtml !!}
+            </div>
+        </nav>
+        <section class="content">
+            <div class="bigimg" style="background-image:url('//picsum.photos/1500/500/')"></div>
+            <div class="container mw-1140">
+                @yield('content')
             </div>
         </section>
+        <footer class="footer">
+            <div class="container mw-1140">
+                &copy; {{ date('Y') }} <a href="https://larapages.nl">LaraPages</a>
+                <a class="right" target="_blank" href="https://github.com/larapages">Visit us on GitHub</a>
+            </div>
+        </footer>
     </body>
 </html>
--- src/views/page.blade.php
+++ src/views/page.blade.php
@@ -1,1 +1,10 @@
 @extends('pages::main')
+
+@section('content')
+    <article class="article">
+        <h1 class="title">{{ $page->title }}</h1>
+        <div class="body">
+            {!! $page->body !!}
+        </div>
+    </article>
+@endsection
