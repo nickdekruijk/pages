773a219b9677755db27e810dd20ab2db7c2b0084
Nick de Kruijk <git@nickdekruijk.nl>
2018-01-26 15:39:58 +0100

    More css tweaks

--- src/views/main.blade.php
+++ src/views/main.blade.php
@@ -13,30 +13,31 @@
         	* {box-sizing:border-box}
         	.content {min-height:calc(100vh - 100px)}
         	.container {position:relative}
-        	.footer .container {padding:0 2px}
-        	.article {padding:1px 2%}
+        	.footer .container {padding:0 20px}
+        	.article {padding:1px 20px}
         	.footer {line-height:100px;background-color:#334;color:rgba(255,255,255,0.8)}
         	A {text-decoration:none;color:inherit}
         	.nav-logo {font-weight:100;font-size:1.5em}
         	.mw-1140 {max-width:1140px;margin-left:auto;margin-right:auto}
-        	.nav-burger {display:none;position:absolute;right:0;padding:11px 2%;z-index:1}
+        	.nav-burger {display:none;position:absolute;right:0;padding:11px 20px;z-index:1}
         	.nav-burger > span {display:block;background-color:#0cb;height:4px;width:22px;margin:4px;border-radius:2px}
         	#nav-toggle {display:none}
         	.nav UL {display:block;margin:0;padding:0}
-        	.nav UL > LI {display:inline-block;margin:0;padding:0px 10px}
-        	.nav UL UL {display:none;position:absolute;background-color:rgba(127,127,127,0.2);margin:0 -10px;line-height:2.5em}
+        	.nav UL > LI {display:inline-block;margin:0;padding:0px 20px}
+        	.nav UL UL {display:none;position:absolute;background-color:rgba(127,127,127,0.2);margin:0 -20px;line-height:2.5em}
         	.nav LI:hover > UL {display:block}
         	.nav LI LI {display:block}
         	.nav LI.active > A {color:#178}
         	.nav A {display:block}
         	.nav LI:hover, .nav-logo:hover {background-color:rgba(0,0,0,0.1)}
-        	.nav-logo {float:left;display:block;padding:0px 2%;color:#178}
+        	.nav-logo {float:left;display:block;padding:0px 20px;color:#178}
         	.right {float:right}
         	.bigimg {height:33.33333vw;background-position:center center;background-size:cover}
         	.footer a {color:#fff}
         	.footer a:hover {text-decoration:underline}
         	h1 {font-size:2em;font-weight:100;margin:0.75em 0 0}
-        	h2 {font-size:1.5em;font-weight:100;margin:0.75em 0 0}
+        	h2 {font-size:1.25em;font-weight:700;margin:0.75em 0 0}
+        	h2 + p {margin-top:0}
         	p {margin:0.5em 0}
             @media (max-width: 750px) {
             	.nav .nav0 {position:fixed;top:0px;bottom:0;right:0;width:200px;transform:translateX(200px);transition:transform .4s;background-color:#eee}
