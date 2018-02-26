081f286b543a10b6a27e4382f98149301d1b42ad
Nick de Kruijk <git@nickdekruijk.nl>
2018-02-26 12:25:56 +0100

    return null if no images

--- src/Images.php
+++ src/Images.php
@@ -6,6 +6,9 @@ trait Images {
 
     public function images($column = 'images', $n = null)
     {
+        if (empty(trim($this->$column))) {
+            return null;
+        }
         $images = array_map('trim', explode(chr(10), trim($this->$column)));
         return $n !== null ? explode('|', $images[$n], 2) : $images;
     }
